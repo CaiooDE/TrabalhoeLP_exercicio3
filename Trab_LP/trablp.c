8b7385e56e238ce81f24db28a92389bcf2b2fa63
CaiooDE <104917012+CaiooDE@users.noreply.github.com>
2023-05-26 12:29:00 -0300

    main modificada trablp.c
    main modificada pra mostrar o uso dos ponteiros acrescendo e decrescendo

--- Trab_LP/trablp.c
+++ Trab_LP/trablp.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 
 typedef struct {
-    void* end;  // Endereço alocado
-    int cont;   // Contagem de ponteiros apontando para o endereço
+    void* end;  // Endereco alocado
+    int cont;   // Contagem de ponteiros apontando para o endereco
 } ContatorReferencia;
 
-static ContatorReferencia* ref = NULL;    // Vetor para rastrear as alocações
+static ContatorReferencia* ref = NULL;    // Vetor para rastrear as alocacoes
 static int ContatorRefTam = 0;            // Tamanho do vetor
 
 
-void* malloc2(size_t size) {	// Função para alocar memória com rastreamento de referências
-    void* end = malloc(size);   // Aloca memória usando o malloc tradicional
+void* malloc2(size_t size) {	// Funcao para alocar memoria com rastreamento de referencias
+    void* end = malloc(size);   // Aloca memoria usando o malloc tradicional
 
     if (end == NULL) {
-        fprintf(stderr, "Erro: Falha na alocação de memória.\n");
+        fprintf(stderr, "Erro: Falha na alocacao de memoria.\n");
         exit(1);
     }
 
-    // Incrementa a contagem de referências
+    // Incrementa a contagem de referencias
     ContatorReferencia* temp = realloc(ref, sizeof(ContatorReferencia) * (ContatorRefTam + 1));
     if (temp == NULL) {
-        fprintf(stderr, "Erro: Falha na alocação de memória.\n");
+        fprintf(stderr, "Erro: Falha na alocacao de memoria.\n");
         exit(1);
     }
 
@@ -33,22 +33,22 @@ void* malloc2(size_t size) {	// Função para alocar memória com rastreamento de r
     return end;
 }
 
-void atrib2(void** dest, void* src) {	// Função para atribuir um novo valor a um ponteiro, atualizando as contagens de referências
+void atrib2(void** dest, void* src) {	// Funcao para atribuir um novo valor a um ponteiro, atualizando as contagens de referencias
     if (dest == NULL) {
-        fprintf(stderr, "Erro: O destino é um ponteiro nulo.\n");
+        fprintf(stderr, "Erro: O destino e um ponteiro nulo.\n");
         exit(1);
     }
 
-    if (*dest != NULL) {	// Decrementa a contagem de referências do ponteiro de destino atual
+    if (*dest != NULL) {	// Decrementa a contagem de referencias do ponteiro de destino atual
         int i;
         for (i = 0; i < ContatorRefTam; i++) {
             if (ref[i].end == *dest) {
                 ref[i].cont--;
 
                 
-                if (ref[i].cont == 0) { 	// Verifica se a contagem de referências chegou a zero
+                if (ref[i].cont == 0) { 	// Verifica se a contagem de referencias chegou a zero
                     free(*dest);
-                    ref[i] = ref[ContatorRefTam - 1];    // Substitui pelo último elemento
+                    ref[i] = ref[ContatorRefTam - 1];    // Substitui pelo ultimo elemento
                     ContatorRefTam--;
                     ref = realloc(ref, sizeof(ContatorReferencia) * ContatorRefTam);    // Reduz o tamanho do vetor
                 }
@@ -60,7 +60,7 @@ void atrib2(void** dest, void* src) {	// Função para atribuir um novo valor a um
 
     *dest = src;	// Atribui o novo ponteiro de origem
 
-    if (src != NULL) {	// Incrementa a contagem de referências do novo ponteiro de origem
+    if (src != NULL) {	// Incrementa a contagem de referencias do novo ponteiro de origem
         int i;
         for (i = 0; i < ContatorRefTam; i++) {
             if (ref[i].end == src) {
@@ -72,48 +72,66 @@ void atrib2(void** dest, void* src) {	// Função para atribuir um novo valor a um
 }
 
 
-void Exibecont() { 	// Função para exibir a contagem de referências de cada endereço alocado
+void Exibecont() { 	// Funcao para exibir a contagem de referencias de cada endereco alocado
     for (int i = 0; i < ContatorRefTam; i++) {
         printf("Contador de referencias: %p = %d\n", ref[i].end, ref[i].cont);
     }
 }
 
 int main() {
-    // Alocação de memória 
+    // Alocacao de memoria 
     int* ptr1 = (int*)malloc2(sizeof(int));
-    *ptr1 = 10;
+    *ptr1 = 1;
 
     int* ptr2 = (int*)malloc2(sizeof(int));
-    *ptr2 = 40;
+    *ptr2 = 2;
 
     int* ptr3 = (int*)malloc2(sizeof(int));
-    *ptr3 = 50;
+    *ptr3 = 3;
 	
 	//Atribuicao de ponteiros 
     int* ptr4 = NULL;
     atrib2((void**)&ptr4, ptr1);
 
     int* ptr5 = NULL;
-    atrib2((void**)&ptr5, ptr1);
+    atrib2((void**)&ptr5, ptr2);
 
     int* ptr6 = NULL;
-    atrib2((void**)&ptr6, ptr1);
+    atrib2((void**)&ptr6, ptr3);
 
-    int* ptr7 = NULL;
-    atrib2((void**)&ptr7, ptr1);
 
-    atrib2((void**)&ptr2, ptr1);
 
-    printf("Coletor automatico de lixo:\n");
-    printf("ptr1: %d\n", *ptr1);
-    printf("ptr2: %d\n", *ptr2);
-    printf("ptr3: %d\n", *ptr3);
+    printf("\nColetor automatico de lixo:\n\n");
     printf("ptr4: %d\n", *ptr4);
     printf("ptr5: %d\n", *ptr5);
-    printf("ptr6: %d\n", *ptr6);
-    printf("ptr7: %d\n", *ptr7);
-    Exibecont();    // Exibe o contador de referências
+    printf("ptr6: %d\n\n", *ptr6);
+    Exibecont();    // Exibe o contador de referencias
 
+  	//Atribuicao de ponteiros (acrscendo e decrescendo o contador) 
+    atrib2((void**)&ptr4, ptr2);
+    atrib2((void**)&ptr5, ptr3);
+    atrib2((void**)&ptr6, ptr3);
+
+  
+    printf("\n\nColetor automatico de lixo 2 instancia:\n\n");
+    printf("ptr4: %d\n", *ptr4);
+    printf("ptr5: %d\n", *ptr5);
+    printf("ptr6: %d\n\n", *ptr6);
+    Exibecont();    // Exibe o contador de referencias
+
+  //Atribuicao de ponteiros (eliminando o espaco alocado no ptr1 por nao ter mais nada apontando pra la) 
+    atrib2((void**)&ptr4, ptr2);
+    atrib2((void**)&ptr5, ptr3);
+    atrib2((void**)&ptr6, ptr3);
+    atrib2((void**)&ptr1, ptr2);
+    atrib2((void**)&ptr2, ptr3);
+
+  
+    printf("\n\nColetor automatico de lixo 3 instancia:\n\n");
+    printf("ptr4: %d\n", *ptr4);
+    printf("ptr5: %d\n", *ptr5);
+    printf("ptr6: %d\n\n", *ptr6);
+    Exibecont();    // Exibe o contador de referencias
+  
     return 0;
 }
-
